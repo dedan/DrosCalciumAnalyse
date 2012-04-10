314c3c9ba774211d449d1eba27e9e1c4b921981f
Stephan Gabler <stephan.gabler@gmail.com>
2012-04-10 20:19:09 +0200

    save the time course integrals
    sum the stimulus response so that it result in a single real value for each stimulus presentation. This value is scaled by the maximum response of the spatial base.

--- microlesions/plot_micro.py
+++ microlesions/plot_micro.py
@@ -1,7 +1,8 @@
 
-import os, pickle
+import os, pickle, json
 import numpy as np
 import pylab as plt
+from NeuralImageProcessing import basic_functions as bf
 
 base_path = '/Users/dedan/projects/fu'
 data_path = os.path.join(base_path, 'data', 'dros_calcium_new', 'mic_split')
@@ -14,20 +15,27 @@ titles = {'lm': 'left, lesioned',
           'rn': 'right, normal'}
 format = 'png'
 
+integrator = bf.StimulusIntegrator(threshold=0)
 results = pickle.load(open(os.path.join(save_path, 'res.pckl')))
+res_integrals = {}
 
 for session in results.keys():
 
-    result = results[session]
     print session
-    fig = plt.figure()
-    fig.suptitle(session)
-
+    result = results[session]
     base_size = result['mo2'].num_objects
     num_bases = len(result['filenames'])
+    integrated = integrator(result['mo2'])
+    res_integrals[session] = {}
+
+    # spatial base plot
+    fig = plt.figure()
+    fig.suptitle(session)
     for modenum in range(base_size):
         single_bases = result['mo2'].base.objects_sample(modenum)
+        res_integrals[session][modenum] = {}
         for base_num in range(num_bases):
+            res_integrals[session][modenum][base_num] = {}
             ax = fig.add_subplot(base_size+1, num_bases, base_num+1)
             ax.imshow(result['mean_bases'][base_num], cmap=plt.cm.gray)
             ax.set_axis_off()
@@ -38,8 +46,16 @@ for session in results.keys():
             data_max = np.max(np.abs(data))
             ax.imshow(data, cmap=plt.cm.hsv, vmin= -data_max, vmax=data_max)
             ax.set_axis_off()
+            ax.set_title('%.2f' % data_max, fontsize=10)
+
+            # temporal base integrals
+            for j, label in enumerate(integrated.label_sample):
+                tmp = integrated.timecourses[j, modenum] * data_max
+                res_integrals[session][modenum][base_num][label] = tmp
+
     plt.savefig(os.path.join(save_path, 'ica_' + session + '.' + format))
 
+    # temporal base plot
     fig = plt.figure()
     for modenum in range(base_size):
         ax = fig.add_subplot(base_size, 1, modenum + 1)
@@ -51,3 +67,5 @@ for session in results.keys():
     plt.savefig(os.path.join(save_path, 'time_' + session + '.' + format))
 
 plt.close('all')
+res_integrals['description'] = 'res_integrals[session][modenum][base_num][label]'
+json.dump(res_integrals, open(os.path.join(inpath, 'integrals.json'), 'w'), indent=2)
