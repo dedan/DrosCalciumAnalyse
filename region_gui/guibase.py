ec1f09ece7d6bdf59b8c7a9669802b1601ff7e39
Stephan Gabler <stephan.gabler@gmail.com>
2012-04-12 11:04:24 +0200

    color the combo boxes, add select and load file functionality

--- region_gui/guibase.py
+++ region_gui/guibase.py
@@ -11,19 +11,33 @@ from layout import Ui_RegionGui # Module generated from reading ui file 'layout.
 class MyGui(QtGui.QMainWindow, Ui_RegionGui):
 
     def __init__(self, parent=None):
+        """initialize the gui, color the boxes, etc.."""
         super(MyGui, self).__init__(parent)
         self.setupUi(self)
+        self.colors = ['#4682B4', '#008080', '#FFA500', '#6B8E23', '#B22222', '#DEB887']
+        self.boxes = [self.ComboBox_1, self.ComboBox_2, self.ComboBox_3,
+                      self.ComboBox_4, self.ComboBox_5, self.ComboBox_6]
+        for i, box in enumerate(self.boxes):
+            box.addItems(['bla', 'blub'])
+            box.setStyleSheet("QComboBox { color: %s; }" % self.colors[i]);
 
+        # connect signals to slots
+        QtCore.QObject.connect(self.SelectButton,
+                               QtCore.SIGNAL("clicked()"),
+                               self.select_file)
+        QtCore.QObject.connect(self.LoadButton,
+                               QtCore.SIGNAL("clicked()"),
+                               self.open_file)
 
-    # def selectroi_example(self):
-    #     #basecanvas area defined in layout
-    #     self.selectcid = self.basecanvas.canvas.mpl_connect('pick_event', self.onpick)
+    def select_file(self):
+        """open file select dialog and enter returned path to the line edit"""
+        fname = QtGui.QFileDialog.getOpenFileName()
+        if fname:
+            self.FilePath.setText(fname)
 
-    # def onpick(self, event):
-    #     event.artist.set_ec('k')
-    #     event.artist.set_linewidth(1)
-    #     self.axbase.figure.canvas.draw()
-    #     self.basecanvas.canvas.mpl_disconnect(self.selectcid)
+    def open_file(self):
+        print self.FilePath.text()
+
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
