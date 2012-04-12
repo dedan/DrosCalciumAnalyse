6cbe2ed694a9adcb1f2ab0a284dd8107cc9910fc
Stephan Gabler <stephan.gabler@gmail.com>
2012-04-12 17:00:01 +0200

    use different colormaps for the overlay plot plus make it a package for the utils import

--- region_gui/guibase.py
+++ region_gui/guibase.py
@@ -8,6 +8,7 @@ from PyQt4 import QtGui
 from NeuralImageProcessing import pipeline
 
 from layout import Ui_RegionGui # Module generated from reading ui file 'layout.ui',
+from DrosCalciumAnalyse import utils
 
 debug = True
 
@@ -26,6 +27,12 @@ class MyGui(QtGui.QMainWindow, Ui_RegionGui):
                       self.ComboBox_4, self.ComboBox_5, self.ComboBox_6]
         self.labels = [self.label_1, self.label_2, self.label_3,
                        self.label_4, self.label_5, self.label_6]
+        self.maps = [utils.create_colormap('iPN', (0., 0., 1.), (1., 1., 0.), (1., 0., 0.)),
+                     utils.create_colormap('vlPrc', (0., 0., 1.), (0., 1., 1.), (0., 1., 0.)),
+                    utils.create_colormap('iPN', (0., 0., 1.), (1., 1., 0.), (1., 0., 0.)),
+                    utils.create_colormap('vlPrc', (0., 0., 1.), (0., 1., 1.), (0., 1., 0.)),
+                     utils.create_colormap('acid', (0., 0., 1.), (1., 0., 0.5), (1., 0., 1.))]
+
 
         for i, label in enumerate(sorted(self.regions.keys())):
             self.boxes[i].addItems(['bla', 'blub'])
@@ -64,12 +71,21 @@ class MyGui(QtGui.QMainWindow, Ui_RegionGui):
         for i in range(self.data.num_objects):
 
             im = bases[i,:,:]
-            im_rgba = plt.cm.jet(im / 2 + 0.5)
+            im_rgba = self.maps[i](im / 2 + 0.5)
             im_rgba[:, :, 3] = 0.8
             im_rgba[np.abs(im) < 0.1, 3] = 0
-            self.SpatialBase.canvas.ax.imshow(im_rgba, aspect='equal', interpolation='nearest')
-            self.SpatialBase.canvas.ax.set_yticks([])
-            self.SpatialBase.canvas.ax.set_xticks([])
+            ax = self.SpatialBase.canvas.fig.add_subplot(self.data.num_objects+1, 1, i+1)
+            ax.contour(im, [0.3], colors=['k'])
+            ax.imshow(im_rgba, aspect='equal', interpolation='nearest')
+            ax.set_yticks([])
+            ax.set_xticks([])
+
+            ax = self.SpatialBase.canvas.fig.add_subplot(self.data.num_objects+1, 1, self.data.num_objects+1)
+            ax.contour(im, [0.3], colors=['k'])
+            ax.imshow(im_rgba, aspect='equal', interpolation='nearest')
+            ax.set_yticks([])
+            ax.set_xticks([])
+            ax.set_title('overlay')
 
             ax = self.TemporalBase.canvas.fig.add_subplot(self.data.num_objects, 1, i)
             ax.plot(self.data.timecourses[:, i])
