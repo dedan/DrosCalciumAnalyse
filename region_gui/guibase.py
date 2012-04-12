7b7c2fab1b461826eab234ce03edbdd29d883120
Stephan Gabler <stephan.gabler@gmail.com>
2012-04-12 15:15:04 +0200

    read a default regions file and add labels to the combo boxes

--- region_gui/guibase.py
+++ region_gui/guibase.py
@@ -1,4 +1,5 @@
-import sys
+import sys, os
+import json
 
 from PyQt4 import QtCore
 from PyQt4 import QtGui
@@ -11,17 +12,26 @@ debug = True
 
 class MyGui(QtGui.QMainWindow, Ui_RegionGui):
 
-    def __init__(self, parent=None):
+    def __init__(self, regions_file, parent=None):
         """initialize the gui, color the boxes, etc.."""
         super(MyGui, self).__init__(parent)
         self.data = pipeline.TimeSeries()
+        self.regions = json.load(open(regions_file))
+
+        # gui init stuff
         self.setupUi(self)
-        self.colors = ['#4682B4', '#008080', '#FFA500', '#6B8E23', '#B22222', '#DEB887']
         self.boxes = [self.ComboBox_1, self.ComboBox_2, self.ComboBox_3,
                       self.ComboBox_4, self.ComboBox_5, self.ComboBox_6]
-        for i, box in enumerate(self.boxes):
-            box.addItems(['bla', 'blub'])
-            box.setStyleSheet("QComboBox { color: %s; }" % self.colors[i]);
+        self.labels = [self.label_1, self.label_2, self.label_3,
+                       self.label_4, self.label_5, self.label_6]
+
+        for i, label in enumerate(sorted(self.regions.keys())):
+            self.boxes[i].addItems(['bla', 'blub'])
+            color = self.regions[label]['color']
+            self.boxes[i].setStyleSheet("QComboBox { color: %s; }" % color)
+            self.labels[i].setStyleSheet("QLabel { color: %s; }" % color)
+            self.labels[i].setText(label)
+
 
         # connect signals to slots
         QtCore.QObject.connect(self.SelectButton,
@@ -30,6 +40,11 @@ class MyGui(QtGui.QMainWindow, Ui_RegionGui):
         QtCore.QObject.connect(self.LoadButton,
                                QtCore.SIGNAL("clicked()"),
                                self.open_file)
+
+        if debug:
+            self.FilePath.setText('/Users/dedan/projects/fu/results/test/onemode/OCO_111018a_nnma.json')
+
+
 
     def select_file(self):
         """open file select dialog and enter returned path to the line edit"""
@@ -57,7 +72,14 @@ class MyGui(QtGui.QMainWindow, Ui_RegionGui):
 
 
 if __name__ == '__main__':
+    # TODO: check whether regions.json is passed as an argument to the script
+    # if not, display a dialog that lets you chose the location of the regions.json
+    # and pass the result of the dialog to the gui.
     app = QtGui.QApplication(sys.argv)
-    my_view = MyGui()
+    if debug:
+        regions_file = '/Users/dedan/projects/fu/dros_calcium/region_gui/test_regions.json'
+    else:
+        regions_file = sys.argv[1]
+    my_view = MyGui(regions_file)
     my_view.show()
     sys.exit(app.exec_())
--- region_gui/mplwidget.py
+++ region_gui/mplwidget.py
@@ -2,15 +2,18 @@ from PyQt4 import QtGui
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import numpy as np
+import pylab as plt
 
 class MplCanvas(FigureCanvas):
 
     def __init__(self):
         self.fig = Figure()
-        self.axes = self.fig.add_subplot(111)
+        axes = plt.Axes(self.fig, [0, 0, 1, 1])
+        self.fig.add_axes(axes)
+        self.ax = self.fig.add_subplot(111)
         x = np.arange(0.0, 3.0, 0.01)
         y = np.cos(2*np.pi*x)
-        self.axes.plot(x, y)
+        self.ax.plot(x, y)
         FigureCanvas.__init__(self, self.fig)
         FigureCanvas.setSizePolicy(self, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
