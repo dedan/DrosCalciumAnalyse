350b6713a2850a54402cf0a6f00c9cee504b474f
Stephan Gabler <stephan.gabler@gmail.com>
2012-06-22 12:03:39 +0200

    revert cluster_modes to a1fb1a0cafd4e987d4
    because the next commit related to cluster_modes (990c68f85a74fa46) deleted lots of code without obvious reason and broke the file

--- cluster_modes.py
+++ cluster_modes.py
@@ -33,7 +33,57 @@ for ind, filename in enumerate(filelist):
     timecourse = np.load(tmp_save + '_time_sica.npy')
 
     num_modes = timecourse.shape[1]
-    
+    stimuli = list(set(names))
+    stimuli.sort()
+    '''
+    try:
+        stimuli.remove('CVA_-1')
+    except ValueError:
+        pass
+    '''
+    ''' +++++++++++++ remove bad modes (low correlation over pseudo-trials(repeated odorstimuli)) +++++++++++++++'''
+
+    # TODO: if more than 2 stimuli, take avarage
+    s1, s2 = [], []
+    splitter = np.split(timecourse, len(names))
+    for i in stimuli:
+        temp = names.index(i)
+        s1.append(splitter[temp])
+        try:
+            s2.append(splitter[names.index(i, temp + 1)])
+        except ValueError:
+            s1.pop()
+    t1, t2 = np.vstack(s1), np.vstack(s2)
+    sel = []
+    for a, b in zip(t1.T, t2.T):
+        simil = np.corrcoef(a, b)[0, 1]
+        sel.append(simil > 0.3)
+    print 'selected modes: ', np.sum(sel)
+    sel = np.array(sel)
+    labels += [os.path.basename(meas_path) + '-' + str(i) for i in np.arange(num_modes)[sel]]
+    timecourse = timecourse.reshape((-1, 20, num_modes))[:, :, sel]
+
+
+    ''' +++++++++++++ calculate mean response of all stimuli +++++++++++++++'''
+    resp = []
+    for stim in stimuli:
+        pos = -1
+        stim_resp = []
+        while True:
+            try:
+                pos = names.index(stim, pos + 1)
+                stim_resp.append(timecourse[pos])
+            except ValueError:
+                break
+        temp = np.mean(np.array(stim_resp), 0)
+        #temp = np.mean(temp[6:12], 0)
+        #temp = temp[5:15]
+        #temp[temp < 0.002] = 0
+        resp.append(temp)
+    resp = np.vstack(resp)
+    all_mean_resp.append(resp)
+all_mean_resp = np.hstack(all_mean_resp)
+
 
 ''' +++++++++++++ cluster modes +++++++++++++++'''
 fig = plt.figure()
