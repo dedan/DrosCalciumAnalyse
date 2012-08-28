b7b8cc68f4b9028181df0d985f60c3613701658c
Stephan Gabler <stephan.gabler@gmail.com>
2012-08-28 16:11:00 +0200

    move timeseries_from_png functionality into runlib

--- runlib.py
+++ runlib.py
@@ -7,9 +7,10 @@ re-used also for the gui
 Created by  on 2012-01-27.
 Copyright (c) 2012. All rights reserved.
 """
-
+import os
 from NeuralImageProcessing import basic_functions as bf
 from NeuralImageProcessing import illustrate_decomposition as vis
+from NeuralImageProcessing.pipeline import TimeSeries
 import numpy as np
 import pylab as plt
 
@@ -21,6 +22,48 @@ sorted_trials = bf.SortBySamplename()
 rel_change = bf.RelativeChange()
 # calculate trial mean
 trial_mean = bf.TrialMean()
+
+def create_timeseries_from_pngs(path, name):
+    '''convert a folder of pngs (created by imageJ) into timeseries objects'''
+
+    files = os.listdir(path)
+    selected_files = [len(i.split('_')) > 2 for i in files]
+    files2 = []
+    for j, i in enumerate(selected_files):
+        if i:
+            files2.append(files[j])
+    files = files2
+
+    frames_per_trial = int(files[0].split('-')[1])
+    frame = np.array([int(i.split('-')[0][2:]) for i in files])
+    point = np.array([int(i.split(' - ')[1][:2]) for i in files])
+    odor = [i.split('_')[1] for i in files]
+    conc = [i.split('_')[2].strip('.tif') for i in files]
+
+    new_odor = []
+    new_conc = []
+    timeseries = []
+    names = []
+
+    for p in set(point):
+        temp = []
+        ind = np.where(point == p)[0]
+        sel_frame = frame[ind]
+        sel_files = [files[ind[i]] for i in np.argsort(sel_frame)]
+        sel_odor = [odor[i] for i in ind]
+        sel_conc = [conc[i] for i in ind]
+        for file in sel_files:
+            im = plt.imread(path + file)
+            temp.append(im.flatten())
+            names.append(file)
+        timeseries.append(np.array(temp))
+        new_odor += sel_odor
+        new_conc += sel_conc
+    shape = im.shape
+    timeseries = np.vstack(timeseries)
+    label = [new_odor[i] + '_' + new_conc[i] for i in range(len(new_odor))]
+    label = [i.strip('.png') for i in label[::frames_per_trial]]
+    return TimeSeries(shape=tuple(shape), series=timeseries, name=name, label_sample=label)
 
 
 def preprocess(ts, config):
