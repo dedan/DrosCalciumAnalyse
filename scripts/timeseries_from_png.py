c0891643a2d32443a6402a3e1a2cae5acc614a15
Stephan Gabler <stephan.gabler@gmail.com>
2012-08-28 15:58:57 +0200

    cleanup and test timeseries_from_png

--- scripts/timeseries_from_png.py
+++ scripts/timeseries_from_png.py
@@ -13,9 +13,7 @@ from NeuralImageProcessing.pipeline import TimeSeries
 
 def create(path):
 
-    dir2analyze = path + 'png/'
-
-    files = os.listdir(dir2analyze)
+    files = os.listdir(path)
     selected_files = [len(i.split('_')) > 2 for i in files]
     files2 = []
     for j, i in enumerate(selected_files):
@@ -54,47 +52,15 @@ def create(path):
     label = [new_odor[i] + '_' + new_conc[i] for i in range(len(new_odor))]
     return timeseries, shape, label
 
-def reorder(timeseries, labels):
-    timeseries_new = []
-    new_label = []
-    for label in set(labels):
-        print label
-        ind = np.array([i == label for i in labels])
-        timeseries_new.append(timeseries[ind, :])
-        new_label += [label] * np.sum(ind)
-    return np.vstack(timeseries_new), new_label
 
-
-#measIDs = ['110901a', '110902a', '111012a', '111013a', '111014a', '111014b']
-#measIDs = ['110817b_neu', '110817c', '110823a', '110823b', '111025a', '111025b']
-#measIDs = ['111017a', '111017b', '111018a', '111018b', '111024a', '111024c', '120111a', '120111b']
-#measIDs = ['111026a', '111027a', '111107a', '111107b', '111108a', '111108b', '120112b']
-#measIDs = ['111117a', '111118a', '111124a']
-#measIDs = ['120307a', '120307b', '120307c', '120308c']
-measIDs = ['111108a', '111108b']
-
-#path = '/home/jan/Documents/dros/new_data/CVA_MSH_ACA_BEA_OCO_align/'
-#path = '/home/jan/Documents/dros/new_data/2PA_PAC_ACP_BUT_OCO_align/'
-#path = '/home/jan/Documents/dros/new_data/OCO_GEO_PAA_ISO_BUT_align/'
-#path = '/home/jan/Documents/dros/new_data/LIN_AAC_ABA_CO2_OCO_align/'
-#path = '/home/jan/Documents/dros/new_data/microlesion_stabilized/'
-#path = '/home/jan/Documents/dros/new_data/micro_align/'
-path = '/home/jan/Documents/dros/new_data/LIN_AAC_ABA_CO2_OCO/'
-#measIDs = os.listdir(path)
-#measIDs.remove('analysis')
-#measIDs.remove('111202a_neu')
 frames_per_trial = 40
 
-for measid in measIDs:
-
-    dataset = path.strip('/').split('/')[-1][:3]
-    pathr = path + measid + '/'
-    path_save = '/home/jan/Documents/dros/new_data/raw/' + dataset + '_' + measid #+ measid + '/unnormed_nnma' + str(variance) + '/'#'percent' + str(variance).split('.')[1] + '/'
-
-    timeseries, shape, label = create(pathr)
-    label = [i.strip('.png') for i in label[::frames_per_trial]]
-    ts = TimeSeries(shape=tuple(shape), series=timeseries, name=measid, label_sample=label)
-    ts.save(path_save)
+measid = '110222a'
+pathr = '/Users/dedan/projects/fu/data/dros_gui_test/110222a/'
+timeseries, shape, label = create(pathr)
+label = [i.strip('.png') for i in label[::frames_per_trial]]
+ts = TimeSeries(shape=tuple(shape), series=timeseries, name=measid, label_sample=label)
+ts.save('/Users/dedan/Desktop/test')
 
 
 
