0a331d58abd7c0d28b336a64658468038992170e
Stephan Gabler <stephan.gabler@gmail.com>
2012-10-03 15:52:36 +0200

    add parameter to select a window for the integrator

--- plot_regions.py
+++ plot_regions.py
@@ -66,6 +66,7 @@ to_turn = ['120112b_neu', '111012a', '111017a_neu', '111018a', '110902a']
 lesion_table_path = '/Users/dedan/Dropbox/results/microcuts.csv'
 lesion_data = 'mic'
 n_frames = 20
+integrator_window = (6, 10)
 
 luts_path = os.path.join(os.path.dirname(__file__), 'colormap_luts')
 filelist = glob.glob(os.path.join(luts_path, '*.lut'))
@@ -117,7 +118,7 @@ filelist = [f for f in filelist if not 'selection' in os.path.basename(f)]
 
 # initialize processing (pipeline) components
 average_over_stimulus_repetitions = bf.SingleSampleResponse()
-integrator = bf.StimulusIntegrator(threshold= -1000)
+integrator = bf.StimulusIntegrator(threshold= -1000, window=integrator_window)
 
 # read lesion-tract table into dictionary for easy access
 if lesion_data:
